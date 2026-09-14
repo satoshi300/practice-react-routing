5ad0b21a31810c0873c9f2bed4d319971e9f9828
Michal Wasiak <michal.wasiak@hotmail.com>
2026-09-14 20:53:10 +1000

    Task 01 done

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Route
+} from 'react-router-dom'
+
+import Home from '../src/components/Home';
+import Contact from '../src/components/Contact'
 
 const Task01 = () => {
     return (
-        <h1>Task01</h1>
+        <Router basename='/task01'>
+            <section><h1>Task01</h1></section>
+            <Route path='/home' component={Home} />
+            <Route path='/contact' component={Contact} />
+        </Router>
     );
 }
 
