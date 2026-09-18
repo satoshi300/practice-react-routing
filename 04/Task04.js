fce4bd2972c1bafbdc46cb88dbe23b9deaea5812
Michal Wasiak <michal.wasiak@hotmail.com>
2026-09-18 14:48:57 +1000

    Task 04 comments removed

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     HashRouter as Router,
     Route,
-    Redirect, useHistory
+    useHistory
 } from 'react-router-dom';
 
-import Shop from '../src/components/Shop';
 import products from '../src/products.json'
 import ShopSort from '../src/components/ShopSort';
 
@@ -15,15 +14,15 @@ const Task04 = () => {
     const handleChange = (e) => {
         history.push('/task04/' + e.target.value)
     }
-    
+
     return (
         <Router basename='/task04'>
             <section>
                 <h1>Task04</h1>
                 <select name="" id="" onChange={handleChange}>
                     <option value="">Sort by</option>
-                    <option value="high">Price high</option>
-                    <option value="low">Price low</option>
+                    <option value="high">Price (High-Low)</option>
+                    <option value="low">Price (Low-High)</option>
                 </select>
             </section>
             <Route path='/:name?'>
